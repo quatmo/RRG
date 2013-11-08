24fc19490bed7a280a3e4021026089c620e40c48
tsubame2002 <tsubame2002@gmail.com>
2013-11-08 09:28:15 +0900

    ちょい配置変更

--- Assets/Scripts/Debug/DebugMode.cs
+++ Assets/Scripts/Debug/DebugMode.cs
@@ -62,52 +62,52 @@ public class DebugMode : MonoBehaviour {
 		if(playerScript.isDebugMode){
 			
 			if(isVisible) {
-				GUI.Box(new Rect(10,10,300,300), "DEBUG MENU");
-				GUI.Label(new Rect(20,50,70,20), "SPEED");
-				if(GUI.Button(new Rect(100,50,40,20), "UP")){
+				GUI.Box(new Rect(10,10,300,200), "");
+				GUI.Label(new Rect(20,20,70,20), "SPEED");
+				if(GUI.Button(new Rect(100,20,40,20), "UP")){
 					DebugManager.runningSpeed += 0.5f;
 				}
-				if(GUI.Button(new Rect(150,50,80,20), "DOWN")){
+				if(GUI.Button(new Rect(150,20,80,20), "DOWN")){
 					DebugManager.runningSpeed -= 0.5f;
 				}
 			
-				GUI.Label(new Rect(20,90,70,20), "JUMP");
-				if(GUI.Button(new Rect(100,90,40,20), "UP")){
+				GUI.Label(new Rect(20,40,70,20), "JUMP");
+				if(GUI.Button(new Rect(100,40,40,20), "UP")){
 					DebugManager.jumpSpeed += 0.5f;
 				}
 				
-				if(GUI.Button(new Rect(150,90,80,20), "DOWN")){
+				if(GUI.Button(new Rect(150,40,80,20), "DOWN")){
 					DebugManager.jumpSpeed -= 0.5f;
 					
 				}
 			
-				GUI.Label(new Rect(20,140,70,20), "GRAVITY");
-				if(GUI.Button(new Rect(100,140,40,20), "UP")){
+				GUI.Label(new Rect(20,60,70,20), "GRAVITY");
+				if(GUI.Button(new Rect(100,60,40,20), "UP")){
 					DebugManager.gravity += 0.5f;
 					
 				}
-				if(GUI.Button(new Rect(150,140,80,20), "DOWN")){
+				if(GUI.Button(new Rect(150,60,80,20), "DOWN")){
 					DebugManager.gravity -= 0.5f;
 				
 				}
-				GUI.Label(new Rect(20,190,80,20), "CAMERA");
-				DebugManager.zoom = GUI.HorizontalSlider(new Rect(80,190,200,20),DebugManager.zoom,2.0f,10.0f );
+				GUI.Label(new Rect(20,80,80,20), "CAMERA");
+				DebugManager.zoom = GUI.HorizontalSlider(new Rect(80,80,200,20),DebugManager.zoom,2.0f,10.0f );
 			
-				if(GUI.Button(new Rect(20,270, 80,20), "RESET")){
+				if(GUI.Button(new Rect(20,180, 80,20), "RESET")){
 					DebugManager.runningSpeed = 4.0f;
 					DebugManager.gravity = 20.0f;
 					DebugManager.jumpSpeed = 8.0f;
 					DebugManager.zoom = 4.0f;
 				}
-				if(GUI.Button(new Rect(110,270,80,20), "RESTART")){
+				if(GUI.Button(new Rect(110,180,80,20), "RESTART")){
 					DebugManager.isInit = true;
 					Application.LoadLevel("Main");
 				}
-				if(GUI.Button(new Rect(200,270,100,20), "EXIT")){
+				if(GUI.Button(new Rect(200,180,100,20), "EXIT")){
 					isVisible = false;
 				}
 			}else{
-				if(GUI.Button(new Rect(100,0,180,30), "DEBUG MENU")){
+				if(GUI.Button(new Rect(100,0,80,30), "DEBUG")){
 					isVisible = true;
 				}
 			
